24a4a30e4e6b97b19d4b50993624c2e78921b9d0
Kelsey <pages.kelsey@gmail.com>
2021-11-28 19:11:22 -0500

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const PORT = process.envPORT || 8080;
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 8085;
 const app = express();
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 module.exports = app;
 
 const createApp = () => {
   // logging middleware
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 
   // body parsing middleware
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(express.static(path.join(__dirname, "..", "public")));
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error('Not found');
+      const err = new Error("Not found");
       err.status = 404;
       next(err);
     } else {
@@ -29,15 +29,15 @@ const createApp = () => {
   });
 
   // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
   });
 
   // error handling endware
   app.use((err, req, res, next) => {
     console.error(err);
     console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Internal server error.');
+    res.status(err.status || 500).send(err.message || "Internal server error.");
   });
 };
 
@@ -47,7 +47,7 @@ const startListening = () => {
     console.log(`Mixing it up on port ${PORT}`)
   );
   const io = socketio(server);
-  require('./socket')(io);
+  require("./socket")(io);
 };
 
 // async function bootApp() {
