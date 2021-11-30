c893c1911c1f928ec3d7f8a753c45fc7ed5797e7
Kelsey <pages.kelsey@gmail.com>
2021-11-30 13:18:04 -0500

    fixed location socket bug

--- server/socket/index.js
+++ server/socket/index.js
@@ -52,36 +52,37 @@ module.exports = (io) => {
         .emit("employeeMoved", officeRooms[roomKey].employees[socket.id]);
     });
 
+    //     //disconnecting
+    //     socket.on("disconnect", () => {
+    //       io.emit("socket disconnected", socket.id);
+
     //disconnecting
-    socket.on('disconnect', () => {
-      io.emit("socket disconnected", socket.id)
-    })
-    // socket.on("disconnect", function () {
-    //   let roomKey = 0;
-    //   for (let keys1 in officeRooms) {
-    //     for (let keys2 in officeRooms[keys1]) {
-    //       Object.keys(officeRooms[keys1][keys2]).map((el) => {
-    //         if (el === socket.id) {
-    //           roomKey = keys1;
-    //         }
-    //       });
-    //     }
-    //   }
+    socket.on("disconnect", function () {
+      let roomKey = 0;
+      for (let keys1 in officeRooms) {
+        for (let keys2 in officeRooms[keys1]) {
+          Object.keys(officeRooms[keys1][keys2]).map((el) => {
+            if (el === socket.id) {
+              roomKey = keys1;
+            }
+          });
+        }
+      }
 
-    //   const roomInfo = officeRooms[roomKey];
+      const roomInfo = officeRooms[roomKey];
 
-    //   if (roomInfo) {
-    //     console.log("user disconnected: ", socket.id);
-    //     //removing from employee object
-    //     delete roomInfo.employees[socket.id];
-    //     //update count
-    //     roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
-    //     io.to(roomKey).emit("disconnected", {
-    //       employeeId: socket.id,
-    //       numEmployees: roomInfo.numEmployees,
-    //     });
-    //   }
-    // });
+      if (roomInfo) {
+        console.log("user disconnected: ", socket.id);
+        //removing from employee object
+        delete roomInfo.employees[socket.id];
+        //update count
+        roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
+        io.to(roomKey).emit("disconnected", {
+          employeeId: socket.id,
+          numEmployees: roomInfo.numEmployees,
+        });
+      }
+    });
 
     socket.on("isKeyValid", function (input) {
       Object.keys(officeRooms).includes(input)
@@ -90,8 +91,8 @@ module.exports = (io) => {
     });
     // get a random code for the room
     socket.on("getRoomCode", async function () {
-      let key = "key";
-      Object.keys(officeRooms).includes(key) ? (key = "key") : key;
+      let key = codeGenerator();
+      Object.keys(officeRooms).includes(key) ? (key = codeGenerator()) : key;
       officeRooms[key] = {
         roomKey: key,
         employees: {},
@@ -99,6 +100,210 @@ module.exports = (io) => {
       };
       socket.emit("roomCreated", key);
     });
-
   });
 };
+
+function codeGenerator() {
+  let code = "";
+  let chars = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < 5; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
+
+// const officeRooms = {};
+
+// const connectedSockets = [];
+
+// module.exports = (io) => {
+//   io.on("connection", (socket) => {
+//     console.log(
+//       `A socket connection to the server has been made: ${socket.id}`
+//     );
+
+//     connectedSockets.push(socket.id);
+//     console.log(connectedSockets);
+//     socket.broadcast.emit("someoneJoined", socket.id);
+
+//     socket.on("joinRoom", (roomKey) => {
+//       socket.join(roomKey);
+//       const roomInfo = officeRooms[roomKey];
+//       console.log("roomInfo", roomInfo);
+//       roomInfo.employees[socket.id] = {
+//         rotation: 0,
+//         x: 400,
+//         y: 300,
+//         employeeId: socket.id,
+//       };
+
+//       roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
+
+//       //set initial state HERE
+//       socket.emit("setState", roomInfo);
+
+//       //sending the employees object to the new employee
+//       socket.emit("currentEmployees", {
+//         employees: roomInfo.employees,
+//         numEmployees: roomInfo.numEmployees,
+//       });
+
+//       //HERE UPDATE ALL COWORKERS OF THE NEW EMPLOYEE
+//       socket.to(roomKey).emit("newEmployee", {
+//         employeeInfo: roomInfo.employees[socket.id],
+//         numEmployees: roomInfo.numEmployees,
+//       });
+//     });
+
+//     // HERE UPDATE THE EMPLOYEES MOVEMENT DATA
+//     socket.on("employeeMovement", function (data) {
+//       const { x, y, roomKey } = data;
+//       officeRooms[roomKey].employees[socket.id].x = x;
+//       officeRooms[roomKey].employees[socket.id].y = y;
+
+//       //emit a message to all employees about the movement of coworker
+//       socket
+//         .to(roomKey)
+//         .emit("employeeMoved", officeRooms[roomKey].employees[socket.id]);
+//     });
+
+//     //disconnecting
+//     socket.on("disconnect", () => {
+//       io.emit("socket disconnected", socket.id);
+//     });
+//     // socket.on("disconnect", function () {
+//     //   let roomKey = 0;
+//     //   for (let keys1 in officeRooms) {
+//     //     for (let keys2 in officeRooms[keys1]) {
+//     //       Object.keys(officeRooms[keys1][keys2]).map((el) => {
+//     //         if (el === socket.id) {
+//     //           roomKey = keys1;
+//     //         }
+//     //       });
+//     //     }
+//     //   }
+
+//     //   const roomInfo = officeRooms[roomKey];
+
+//     //   if (roomInfo) {
+//     //     console.log("user disconnected: ", socket.id);
+//     //     //removing from employee object
+//     //     delete roomInfo.employees[socket.id];
+//     //     //update count
+//     //     roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
+//     //     io.to(roomKey).emit("disconnected", {
+//     //       employeeId: socket.id,
+//     //       numEmployees: roomInfo.numEmployees,
+//     //     });
+//     //   }
+//     // });
+
+//     socket.on("isKeyValid", function (input) {
+//       Object.keys(officeRooms).includes(input)
+//         ? socket.emit("keyIsValid", input)
+//         : socket.emit("keyNotValid");
+//     });
+
+//  const officeRooms = {};
+
+// const connectedSockets = [];
+
+// module.exports = (io) => {
+//   io.on("connection", (socket) => {
+//     console.log(
+//       `A socket connection to the server has been made: ${socket.id}`
+//     );
+
+//     connectedSockets.push(socket.id);
+//     socket.broadcast.emit("someoneJoined", socket.id);
+
+//     socket.on("joinRoom", (roomKey) => {
+//       socket.join(roomKey);
+//       const roomInfo = officeRooms[roomKey];
+//       console.log("roomInfo", roomInfo);
+//       roomInfo.employees[socket.id] = {
+//         rotation: 0,
+//         x: 400,
+//         y: 300,
+//         employeeId: socket.id,
+//       };
+
+//       roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
+
+//       //set initial state HERE
+//       socket.emit("setState", roomInfo);
+
+//       //sending the employees object to the new employee
+//       socket.emit("currentEmployees", {
+//         employees: roomInfo.employees,
+//         numEmployees: roomInfo.numEmployees,
+//       });
+
+//       //HERE UPDATE ALL COWORKERS OF THE NEW EMPLOYEE
+//       socket.to(roomKey).emit("newEmployee", {
+//         employeeInfo: roomInfo.employees[socket.id],
+//         numEmployees: roomInfo.numEmployees,
+//       });
+//     });
+
+//     // HERE UPDATE THE EMPLOYEES MOVEMENT DATA
+//     socket.on("employeeMovement", function (data) {
+//       const { x, y, roomKey } = data;
+//       officeRooms[roomKey].employees[socket.id].x = x;
+//       officeRooms[roomKey].employees[socket.id].y = y;
+
+//       //emit a message to all employees about the movement of coworker
+//       socket
+//         .to(roomKey)
+//         .emit("employeeMoved", officeRooms[roomKey].employees[socket.id]);
+//     });
+
+//     //disconnecting
+//     socket.on('disconnect', () => {
+//       io.emit("socket disconnected", socket.id)
+//     })
+//     // socket.on("disconnect", function () {
+//     //   let roomKey = 0;
+//     //   for (let keys1 in officeRooms) {
+//     //     for (let keys2 in officeRooms[keys1]) {
+//     //       Object.keys(officeRooms[keys1][keys2]).map((el) => {
+//     //         if (el === socket.id) {
+//     //           roomKey = keys1;
+//     //         }
+//     //       });
+//     //     }
+//     //   }
+
+//     //   const roomInfo = officeRooms[roomKey];
+
+//     //   if (roomInfo) {
+//     //     console.log("user disconnected: ", socket.id);
+//     //     //removing from employee object
+//     //     delete roomInfo.employees[socket.id];
+//     //     //update count
+//     //     roomInfo.numEmployees = Object.keys(roomInfo.employees).length;
+//     //     io.to(roomKey).emit("disconnected", {
+//     //       employeeId: socket.id,
+//     //       numEmployees: roomInfo.numEmployees,
+//     //     });
+//     //   }
+//     // });
+
+//     socket.on("isKeyValid", function (input) {
+//       Object.keys(officeRooms).includes(input)
+//         ? socket.emit("keyIsValid", input)
+//         : socket.emit("keyNotValid");
+//     });
+//     // get a random code for the room
+//     socket.on("getRoomCode", async function () {
+//       let key = "key";
+//       Object.keys(officeRooms).includes(key) ? (key = "key") : key;
+//       officeRooms[key] = {
+//         roomKey: key,
+//         employees: {},
+//         numEmployees: 0,
+//       };
+//       socket.emit("roomCreated", key);
+//     });
+
+//   });
