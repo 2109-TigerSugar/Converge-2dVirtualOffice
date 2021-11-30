496ff44dbe21ba991381c22b203eb65b07c7b35f
Hyo Kim <hyo.kim968@gmail.com>
2021-11-30 14:33:52 -0800

    create and join form on the landing page

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from 'react';
 
 // export const CreateForm = () => {
 //   const [roomKey, setRoomKey] = useState('')
@@ -24,38 +23,76 @@ import React, { useState } from "react";
 //   )
 // }
 
-
 export const JoinOrCreateForm = (props) => {
   const [userData, setUserData] = useState({
-    name:"",
-    roomKey: "",
-    officeType: "",
-  })
+    name: '',
+    roomKey: '',
+    officeType: '',
+  });
 
-useEffect(() => {
-  window.localStorage.getItem("userData")
-},[])
+  // only fetch from localStorage if formType is 'join'
 
+  useEffect(() => {
+    console.log('useEffect Ran');
+    if (props.formType === 'join') {
+      let storedData = window.localStorage.getItem('userData');
+      console.log(storedData);
+      if (storedData) {
+        console.log(storedData);
+        setUserData(JSON.parse(storedData));
+      }
+    } else {
+      setUserData({
+        name: '',
+        roomKey: '',
+        officeType: '',
+      });
+    }
+  }, [props.formType]);
 
   const handleChange = (event) => {
-    setUserData({...userData, [event.target.name]: event.target.value})
-  }
+    setUserData({ ...userData, [event.target.name]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(userData)
-  }
+    event.preventDefault();
+    console.log(userData);
 
+    window.localStorage.setItem('userData', JSON.stringify(userData));
+    setUserData({
+      name: '',
+      roomKey: '',
+      officeType: '',
+    });
+  };
 
-  return(
+  return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" value={userData.name} onChange= {handleChange}/>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={userData.name}
+        onChange={handleChange}
+      />
       <label htmlFor="officeKey">Room Key</label>
-      <input type="text" name="roomKey" id="officeKey" value={userData.roomKey} onChange= {handleChange}/>
+      <input
+        type="text"
+        name="roomKey"
+        id="officeKey"
+        value={userData.roomKey}
+        onChange={handleChange}
+      />
       <label htmlFor="officeType">Office Type</label>
-      <input type="text" name="officeType" id="officeType" value={userData.officeType} onChange= {handleChange}/>
+      <input
+        type="text"
+        name="officeType"
+        id="officeType"
+        value={userData.officeType}
+        onChange={handleChange}
+      />
       <button type="submit">{props.formType}</button>
     </form>
-  )
-}
+  );
+};
