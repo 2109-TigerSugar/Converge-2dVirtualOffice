0ea0d7f92cef6977e54e3866798eeff7d777cf80
Hyo Kim <hyo.kim968@gmail.com>
2021-11-30 12:38:04 -0800

    forms component

---
+++ src/components/Forms.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const CreateForm = () => {
+  return 'create form';
+}
+
+
+export const JoinForm = () => {
+  return 'join form';
+}
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,20 @@
-import React from "react";
-
+import React from 'react';
+import { CreateForm, JoinForm } from './Forms';
 
 const LandingPage = () => {
-  return 'landing page'
-}
+  return (
+    // 2 buttons: 1 for create 1 for join
+    <div className="landing-page">
+      <div className="buttons">
+        <button type="button">Create</button>
+        <button type="button">Join</button>
+      </div>
+      <div className="form">
+        <CreateForm />
+        <JoinForm />
+      </div>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
