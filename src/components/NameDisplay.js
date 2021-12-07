3e29e8cc0ee8ac82933db09e9a12515cbe6e3b94
Lydia Rees <lydiairis76@gmail.com>
2021-12-07 11:51:17 -0600

    joined and left messages working

--- src/components/NameDisplay.js
+++ src/components/NameDisplay.js
@@ -2,44 +2,50 @@ import React, { useEffect, useState } from 'react';
 
 import { socket } from '../socket';
 
+// const initialState = [{
+//   employeeid: null,
+//   name:''
+// }]
 
 const NameDisplay = () => {
   console.log(socket);
 
 
-  const [name, setNames] = useState([]);
+  const [names, setNames] = useState([]);
   const [isActive, setActive] = useState(true);
 
+  // const clearState = () => {
+  //   setNames([...initialState])
+  //    }
 
   useEffect(() => {
 
-    socket.on("newEmployee", function(arg) {
-      const { employeeInfo } = arg
-      setNames([...name, employeeInfo.name + " joined the room"])
-      console.log('new', employeeInfo)
+    socket.on("newEmployee", function(data) {
+      const { coworkerName } = data
+      setNames([...names, coworkerName + " joined"])
 
     })
 
-    // socket.on("coworkerLeftRoom", function(arg) {
-    //   const
-    // })
+    socket.on("coworker disconnected", function(data) {
+      const { coworkerName } = data
+      setNames([...names, coworkerName + " left"])
+    })
 
-
-  }, );
-
-
-  useEffect(() => {
     const timer = setTimeout(() => {
-       setActive(false)
-    }, 10000);
-    return () => clearTimeout(timer);
- }, []);
+             setNames(names.slice(1))
+          }, 5000);
+          return () => clearTimeout(timer);
+  },);
+
+
 
   return(
     <div>
-    <div className={isActive ? "nameJoined" : "noNameJoined"}>
-    <p>{name}</p>
-    </div>
+      <div className={isActive ? "nameJoined" : "noNameJoined"}>
+       {names.map((name, index) => {
+         return <div key={index}>{name}</div>
+       })}
+      </div>
     </div>
 
   )
