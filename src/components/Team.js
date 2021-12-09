24b111c46905df4ac090684687acab3c21924947
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-12-09 12:21:50 -0500

    Create Team component

---
+++ src/components/Team.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { hidePanels } from '../helperFunctions';
+import TeamMember from './TeamMember';
+
+const Team = () => {
+  hidePanels();
+  return (
+    <div className="team">
+      <div id="team-container">
+        <TeamMember></TeamMember>
+        <TeamMember></TeamMember>
+        <TeamMember></TeamMember>
+        <TeamMember></TeamMember>
+      </div>
+    </div>
+  );
+};
+export default Team;
