d3bd5481b1f0b2e15134baf5cd08db4acc2cb91f
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-12-09 14:50:28 -0500

    Add avatar and names to team member component

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+//Image
+//Name
+//LinkedIn | GitHub
+//Custom Avatar
+
 const TeamMember = props => {
-  return <div className="team-member"></div>;
+  const { imageSrc, name, linkedIn, github, avatarSrc } = props;
+  return (
+    <div className="team-member">
+      <div className="member-photo">
+        <img src={imageSrc} />
+      </div>
+      <div className="member-name">FirstName LastName</div>
+
+      <div className="member-social"></div>
+      <div className="member-avatar">
+        <img src="assets/kelsey-avatar.png" />
+      </div>
+    </div>
+  );
 };
 
 export default TeamMember;
