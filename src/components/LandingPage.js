82447e1d8c15f1deda54443397eec91b9c7a6840
Hyo Kim <hyo.kim968@gmail.com>
2021-12-10 09:45:03 -0800

    added work

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -39,7 +39,7 @@ const LandingPage = () => {
         </p>
       </div>
       <div className="button-hero">
-        <img src="../assets/work.gif" />
+        <img src="../assets/work2.gif" />
         <div className="buttonsAndForm">
           <div className={isActive ? 'buttons' : 'noButtons'}>
             <button onClick={handleClick} type="button" id="create">
