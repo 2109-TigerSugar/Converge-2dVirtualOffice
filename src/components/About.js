c89f817f0ca8f4774d18e48b87572a524d1f8ee7
Kelsey <pages.kelsey@gmail.com>
2021-12-10 10:29:11 -0500

    about page more styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,27 +4,64 @@ import { hidePanels } from '../helperFunctions';
 const About = () => {
   hidePanels();
   return (
-    <div>
-      <div className="intro">
-        <h4>Working remotely doesn't have to be boring.</h4>
+    <div className="about">
+      <div id="team-header">
+        <div className="about-gif">
+          <img src="../assets/player_walk.gif" />
+          <h4>Working remotely doesn't have to be boring.</h4>
+          <img src="../assets/chair.gif" />
+        </div>
       </div>
-      <div className="about">
-        <p>
-          Converge allows you have all the good things about working in person,
-          and still enjoy not dealing with the cons.
-        </p>
-        <p className="about-right">
-          No more hassle with your commute.
-          <img src="../assets/traffic.gif" />
-        </p>
-        <span>
-          {' '}
-          <p className="about-left">
-            Different work environments allows you to signal to coworkers your
-            availability. <img src="../assets/about.png" />
-          </p>
-        </span>
-        <span> </span>
+
+      <div id="team-container">
+        <div className="team-member">
+          <div className="member-photo">
+            <img src="../assets/about.png" />
+            <p>
+              {' '}
+              Different work environments allows you to signal to coworkers your
+              availability.
+            </p>
+          </div>
+        </div>
+
+        <div id="team-container">
+          <div className="team-member">
+            <div className="member-photo">
+              <p>
+                Converge allows you have all the good things about working in
+                person, and still enjoy not dealing with the cons.
+              </p>
+              <p>
+                <img src="../assets/traffic.jpg" />
+                No more hassle with your commute.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div id="team-container">
+          <div className="team-member">
+            <div className="member-photo">
+              <video autoPlay muted loop>
+                <source src="assets/howto.mp4" type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
+        <div className="team-member">
+          <div className="member-photo">
+            <img src="assets/instruction2.jpg" />
+          </div>
+        </div>
+        <div id="team-container">
+          <div className="team-member">
+            <div className="member-video">
+              <video autoPlay muted loop>
+                <source src="assets/walkaway.mp4" type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
