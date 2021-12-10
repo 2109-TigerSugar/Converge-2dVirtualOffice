211f9be34ee799c9bdb2586799251026a08994ca
Hyo Kim <hyo.kim968@gmail.com>
2021-12-10 07:43:34 -0800

    error handling

--- src/webcam.js
+++ src/webcam.js
@@ -3,9 +3,8 @@ import adapter from 'webrtc-adapter';
 import { addVideo, createPeer } from './helperFunctions';
 
 const runWebRTC = async (socket, myName) => {
-
   socket.on('newEmployee', ({ employeeInfo }) => {
-    if(window.peer === undefined) {
+    if (window.peer === undefined) {
       console.log('missed calls', employeeInfo.employeeId);
     }
   });
@@ -18,11 +17,15 @@ const runWebRTC = async (socket, myName) => {
     console.log('stream exists', stream);
   } else {
     //Dakota: Ask for permission to use webcam :) We await because we have no clue when they will accept it!
-    stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    window.myStream = stream;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      window.myStream = stream;
+    } catch (err) {
+      console.log('error in getting stream', err);
+    }
   }
 
   // attach stream to window so we can use in Office.js
@@ -46,7 +49,7 @@ const runWebRTC = async (socket, myName) => {
       call.on('stream', remoteStream => {
         if (callList[call.peer] === undefined) {
           addVideo(remoteStream, true, call.peer);
-          callList[call.peer] = true;
+          callList[call.peer] = call;
         }
       });
     });
@@ -64,7 +67,7 @@ const runWebRTC = async (socket, myName) => {
           call.on('stream', remoteStream => {
             if (callList[socketId] === undefined) {
               addVideo(remoteStream, true, socketId, employeeInfo.name);
-              callList[socketId] = true;
+              callList[socketId] = call;
             }
           });
           console.log('timer stopped');
@@ -77,10 +80,18 @@ const runWebRTC = async (socket, myName) => {
 
     // when someone leave the office, remove that video
     socket.on('coworker disconnected', ({ coworkerId: socketId }) => {
-      let videoToRemove = document.querySelectorAll(`div#${CSS.escape(socketId)}`);
+      let videoToRemove = document.querySelectorAll(
+        `div#${CSS.escape(socketId)}`
+      );
       videoToRemove.forEach(video => video.remove());
+      callList[socketId] && callList[socketId].close();
       delete callList[socketId];
     });
+  });
+
+  peer.on('error', err => {
+    console.log('peer connection error', err);
+    peer.reconnect();
   });
 };
 
